e0f4da8ad0fb4837b900c1131acfb8fdc23b0a34
rex <rex>
2023-07-03 23:31:22 +0800

    1.新增popup action 2.新增 code formater

--- content.js
+++ content.js
@@ -1,80 +1,76 @@
-console.log("content.js")
-let phase1 = false
-let phase2 = false
-let phase3 = false
+console.log("content.js");
+let phase1 = false;
+let phase2 = false;
+let phase3 = false;
 //append html tag
-function append_bubble(className,content) {
-    if (phase1 === false) {
-        phase1 = true
-    }
-
-    const bubble_body = document.createElement("div")
-    bubble_body.setAttribute("class", "bubble_body")
-    const bubble = document.createElement("div")
-    bubble.setAttribute("class", "bubble right")
-    const ptag = document.createElement("p")
-    ptag.setAttribute("class", className)
-    ptag.textContent = content
-
-    bubble_body.appendChild(bubble)
-    bubble.appendChild(ptag)
-    document.body.appendChild(bubble_body)
-  }
-  
-  function append_bubble2(className,content) {
-    if (phase2 === false) {
-        phase2 = true
-    }
-    const bubble_body = document.querySelector(".bubble_body")
-    const bubble = document.createElement("div")
-    bubble.setAttribute("class", "bubble right")
-    const ptag = document.createElement("p")
-    ptag.setAttribute("class", className)
-    ptag.textContent = content
-    bubble.appendChild(ptag)
-    bubble_body.appendChild(bubble)
+function append_bubble(className, content) {
+  if (phase1 === false) {
+    phase1 = true;
   }
 
-  function append_bubble3(className,content) {
-    if (phase3 === false) {
-        phase3 = true
-    }
-    const bubble_body = document.querySelector(".bubble_body")
-    const bubble = document.createElement("div")
-    bubble.setAttribute("class", "bubble right")
-    const ptag = document.createElement("p")
-    ptag.setAttribute("class", className)
-    ptag.textContent = content
-    bubble.appendChild(ptag)
-    bubble_body.appendChild(bubble)
+  const bubble_body = document.createElement("div");
+  bubble_body.setAttribute("class", "bubble_body");
+  const bubble = document.createElement("div");
+  bubble.setAttribute("class", "bubble right");
+  const ptag = document.createElement("p");
+  ptag.setAttribute("class", className);
+  ptag.textContent = content;
+
+  bubble_body.appendChild(bubble);
+  bubble.appendChild(ptag);
+  document.body.appendChild(bubble_body);
+}
+
+function append_bubble2(className, content) {
+  if (phase2 === false) {
+    phase2 = true;
   }
-  
+  const bubble_body = document.querySelector(".bubble_body");
+  const bubble = document.createElement("div");
+  bubble.setAttribute("class", "bubble right");
+  const ptag = document.createElement("p");
+  ptag.setAttribute("class", className);
+  ptag.textContent = content;
+  bubble.appendChild(ptag);
+  bubble_body.appendChild(bubble);
+}
+
+function append_bubble3(className, content) {
+  if (phase3 === false) {
+    phase3 = true;
+  }
+  const bubble_body = document.querySelector(".bubble_body");
+  const bubble = document.createElement("div");
+  bubble.setAttribute("class", "bubble right");
+  const ptag = document.createElement("p");
+  ptag.setAttribute("class", className);
+  ptag.textContent = content;
+  bubble.appendChild(ptag);
+  bubble_body.appendChild(bubble);
+}
 
 //監聽來自background的訊息
-chrome.runtime.onConnect.addListener(function(port){
-    port.onMessage.addListener(function(response) {
-        console.log("我收到了")
-        console.log(response.msg)
-        //phase 1
-        if (response.msg > 300) {
-            if (phase1 === false) {
-            append_bubble("content", "You are getting distract")
-            }
-        }
-        //phase 2 
-        if (response.msg > 600) {
-            if (phase2 === false) {
-            append_bubble2("content", "You really getting distract")
-            }
-        }
-        //phase3
-        if (response.msg > 900) {
-            if (phase3 === false) {
-            append_bubble3("content", "幹 你要分心多久")
-            }
-        }
-        
-    })
-
+chrome.runtime.onConnect.addListener(function (port) {
+  port.onMessage.addListener(function (response) {
+    console.log("我收到了");
+    console.log(response.msg);
+    //phase 1
+    if (response.msg > 300) {
+      if (phase1 === false) {
+        append_bubble("content", "You are getting distract");
+      }
+    }
+    //phase 2
+    if (response.msg > 600) {
+      if (phase2 === false) {
+        append_bubble2("content", "You really getting distract");
+      }
+    }
+    //phase3
+    if (response.msg > 900) {
+      if (phase3 === false) {
+        append_bubble3("content", "幹 你要分心多久");
+      }
+    }
+  });
 });
-
--- service-worker.js
+++ service-worker.js
@@ -1,63 +1,63 @@
-import {focused, genericOnClick, linstenOninstalled}from './script/context_menu.js';
-import { Timer } from './model/timer.js';
+import {
+  focused,
+  genericOnClick,
+  linstenOninstalled,
+} from "./script/context_menu.js";
+import { Timer } from "./model/timer.js";
 chrome.contextMenus.onClicked.addListener(genericOnClick);
 linstenOninstalled();
 //變數
-let focusTabId = null
-let currentTabId = null // 存儲當前活動的 tab 的 ID
-let timer = new Timer(0)
-let port = null
-let intervalID = null
+let focusTabId = null;
+let currentTabId = null; // 存儲當前活動的 tab 的 ID
+let port = null;
+let timer = new Timer(0);
+let intervalID = null;
 
 //defineProperty
-Object.defineProperty(chrome.windows, 'focused', {
+Object.defineProperty(chrome.windows, "focused", {
   get: getFocused,
-  set: setMyVariable
+  set: setMyVariable,
 });
 function getFocused() {
   return focused;
 }
 function setMyVariable(value) {
   if (value === false) {
-    focusTabId = null
+    focusTabId = null;
   } else {
-    focusTabId = currentTabId
+    focusTabId = currentTabId;
   }
-  timer.stopTimer()
+  timer.stopTimer();
 }
 
-
-
-
 // 監聽 active tab 切換事件
-chrome.tabs.onActivated.addListener(function(activeInfo) {
-  chrome.tabs.get(activeInfo.tabId, function(tab) {
+chrome.tabs.onActivated.addListener(function (activeInfo) {
+  chrome.tabs.get(activeInfo.tabId, function (tab) {
     let url = tab.url;
     if (url.startsWith("https") || url.startsWith("http")) {
       currentTabId = tab.id;
     }
-    console.log("focusTabId: ",focusTabId)
-    console.log("tab.id: ",tab.id)
+    console.log("focusTabId: ", focusTabId);
+    console.log("tab.id: ", tab.id);
     switch (focusTabId) {
       case null:
-        break
+        break;
       case tab.id:
-        break
+        break;
       default:
         let url = tab.url;
         console.log("當前標籤頁的 URL：" + tab.url);
-        timer.resetTime()
+        timer.resetTime();
         //如果先前已建立連接，要斷開
         if (currentTabId && port) {
-          port.disconnect()
-          timer.startTimer(connectToCurrentTab)
+          port.disconnect();
+          timer.startTimer(connectToCurrentTab);
         }
         if (url.startsWith("https") || url.startsWith("http")) {
-          timer.startTimer(connectToCurrentTab)
-            currentTabId = activeInfo.tabId;
-            port = chrome.tabs.connect(currentTabId);
-            console.log(currentTabId)
-          
+          timer.startTimer(connectToCurrentTab);
+          currentTabId = activeInfo.tabId;
+          port = chrome.tabs.connect(currentTabId);
+          console.log(currentTabId);
         }
     }
   });
@@ -65,50 +65,46 @@ chrome.tabs.onActivated.addListener(function(activeInfo) {
 
 // 建立連結後的開始跑回圈
 function connectToCurrentTab() {
-    timer.plusTime()
-    if (port && port.sender && port.sender.tab ) {
-      port.postMessage({ msg: timer.getTime });
-    } else {
-      port = chrome.tabs.connect(currentTabId);
-      port.postMessage({ msg: timer.getTime });
-      
-    }
+  timer.plusTime();
+  if (port && port.sender && port.sender.tab) {
+    port.postMessage({ msg: timer.getTime });
+  } else {
+    port = chrome.tabs.connect(currentTabId);
+    port.postMessage({ msg: timer.getTime });
+  }
 }
 
 // 當tab跳轉到網頁時開始計時
-chrome.tabs.onUpdated.addListener(function(tabId, changeInfo, tab) {  
+chrome.tabs.onUpdated.addListener(function (tabId, changeInfo, tab) {
   if (tab.url.startsWith("https") || tab.url.startsWith("http")) {
     currentTabId = tabId;
   }
-  console.log("focusTabId: ",focusTabId)
-  console.log("tabId",tabId)
+  console.log("focusTabId: ", focusTabId);
+  console.log("tabId", tabId);
   switch (focusTabId) {
     case null:
-      break
+      break;
     case tabId:
-      break
+      break;
     default:
       if (currentTabId != tabId) {
-        currentTabId = tabId
-        timer.resetTime()
+        currentTabId = tabId;
+        timer.resetTime();
         if (currentTabId && port) {
-          port.disconnect()
-          timer.stopTimer()
+          port.disconnect();
+          timer.stopTimer();
         }
-    }
-
-    if (tab.url.startsWith("https") || tab.url.startsWith("http")) {
-      switch (changeInfo.status) {
-        case "complete":
-            console.log("conmplete")
-            timer.startTimer(connectToCurrentTab)
-          currentTabId = tabId;
-          port = chrome.tabs.connect(currentTabId);
-        default:
-          break
-      }  
-    }
+      }
+      if (tab.url.startsWith("https") || tab.url.startsWith("http")) {
+        switch (changeInfo.status) {
+          case "complete":
+            console.log("conmplete");
+            timer.startTimer(connectToCurrentTab);
+            currentTabId = tabId;
+            port = chrome.tabs.connect(currentTabId);
+          default:
+            break;
+        }
+      }
   }
-    
 });
-
